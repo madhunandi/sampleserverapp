a821e7db69dafef40ae4e90558cb0c866d1cf57c
MadhuNandi <nandimadhu78@gmail.com>
2018-11-01 10:17:54 +0800

    inclusion of application deploy script

--- scripts/commit_check.sh
+++ scripts/commit_check.sh
@@ -3,10 +3,8 @@ set -e
 # latest commit
 LATEST_COMMIT=$(git rev-parse HEAD)
 
-# latest commit where path/to/folder1 was changed
 FOLDER1_COMMIT=$(git log -1 --format=format:%H --full-diff ./public)
 
-# latest commit where path/to/folder2 was changed
 FOLDER2_COMMIT=$(git log -1 --format=format:%H --full-diff ./src)
 
 if [ $FOLDER1_COMMIT = $LATEST_COMMIT ];
@@ -15,6 +13,7 @@ if [ $FOLDER1_COMMIT = $LATEST_COMMIT ];
 elif [ $FOLDER2_COMMIT = $LATEST_COMMIT ];
     then
         echo "files in folder2 has changed"
+        ./scripts/application.sh
 else
      echo "no folders of relevance has changed"
      exit 0;
